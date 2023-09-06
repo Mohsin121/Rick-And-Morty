8726f6cb83c1416aeba9c20fba0d09133fcd0877
Mohsin121 <mhsnmubeen@gmail.com>
2023-09-06 15:58:09 +0500

    update fixes

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -4,6 +4,11 @@ import { Link } from 'react-router-dom';
 const List = () => {
   const [characters, setCharacters] = useState([]);
   const [searchText, setSearchText] = useState('');
+  const [checkboxes, setCheckboxes] = useState({
+    alive: false,
+    dead: false,
+    unknown: false,
+  });
   const [isLoading, setIsLoading] = useState(true);
 
   function handleChange(event) {
@@ -11,7 +16,14 @@ const List = () => {
     setSearchText(newSearchText);
   }
 
-  const getCharcters = () => {
+  const handleCheckbox = (name) => {
+    setCheckboxes((prevState) => ({
+      ...prevState,
+      [name]: !prevState[name],
+    }));
+  };
+
+  const getCharacters = () => {
     const apiUrl = 'https://rickandmortyapi.com/api/character';
 
     fetch(apiUrl)
@@ -33,44 +45,129 @@ const List = () => {
   };
 
   useEffect(() => {
-    getCharcters();
+    getCharacters();
   }, []);
 
-  const filteredCharacters = characters.filter((character) =>
-    character.name.toLowerCase().includes(searchText.toLowerCase())
-  );
+  const filterByStatus = characters.filter((character) => {
+    if (checkboxes.alive && character.status === 'Alive') {
+      return true;
+    }
+    if (checkboxes.dead && character.status === 'Dead') {
+      return true;
+    }
+    if (checkboxes.unknown && character.status === 'unknown') {
+      return true;
+    }
+    return false;
+  });
+
+  const filteredCharacters = characters
+    .filter((character) =>
+      character.name.toLowerCase().includes(searchText.toLowerCase())
+    )
+    .sort((a, b) => a.name.localeCompare(b.name));
+
+  const filteredAndSortedCharacters =
+    checkboxes.alive || checkboxes.dead || checkboxes.unknown
+      ? filterByStatus
+      : filteredCharacters;
 
   return (
-    <div>
-      <h2 className="title">Search by Title</h2>
-      <div className="input-container">
-        <input
-          type="text"
-          className="input-size"
-          value={searchText}
-          onChange={handleChange}
-          placeholder="Search by name"
-        />
+    <>
+      <div className="container">
+        <h1 className="title">Rick and Morty</h1>
+
+        <div className="input-container">
+          <div className="d-flex align-items-center flex-wrap gap-5 mb-4">
+            <div className="d-flex flex-column">
+              <h3 className="section-title">Search by Title</h3>
+
+              <input
+                type="text"
+                className="input-size mb-0"
+                value={searchText}
+                onChange={handleChange}
+                placeholder="Search by name"
+              />
+            </div>
+
+            <div className="input-container mt-0">
+              <h3 className="section-title">Search by Status</h3>
+              <div className="checkbox ">
+                <label>
+                  <input
+                    type="checkbox"
+                    name="alive"
+                    value="alive"
+                    checked={checkboxes.alive}
+                    onChange={() => handleCheckbox('alive')}
+                  />
+                  Alive
+                </label>
+
+                <label>
+                  <input
+                    type="checkbox"
+                    name="dead"
+                    value="dead"
+                    checked={checkboxes.dead}
+                    onChange={() => handleCheckbox('dead')}
+                  />
+                  Dead
+                </label>
+
+                <label>
+                  <input
+                    type="checkbox"
+                    name="unknown"
+                    value="unknown"
+                    checked={checkboxes.unknown}
+                    onChange={() => handleCheckbox('unknown')}
+                  />
+                  Unknown
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
       {isLoading ? (
         <p>Loading data...</p>
       ) : (
-        <div className="card-container">
-          {filteredCharacters.map((item) => (
-            <div className="card" key={item.id}>
-              {' '}
-              <Link to={`/details/${item.id}`}>
-                {' '}
-                <h2 className="item-name"><span>{item.name}</span></h2>
-              </Link>
-              <img src={item.image} alt={item.name} />
-              <p><strong>Status: </strong>{item.status}</p>
-              <p><strong>Episodes: </strong>{item.episode.length}</p>
-            </div>
-          ))}
-        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Image</th>
+              <th>Status</th>
+              <th>Episodes</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredAndSortedCharacters.map((item) => (
+              <tr key={item.id}>
+                <td>
+                  <Link to={`/details/${item.id}`}>
+                    <span>{item.name}</span>
+                  </Link>
+                </td>
+                <td>
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    height={50}
+                    width={50}
+                  />
+                </td>
+                <td>{item.status}</td>
+                <td>{item.episode.length}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
-    </div>
+    </>
   );
 };
 
